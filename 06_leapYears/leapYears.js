83faf9836e19147ea15fc418001c5bc84dfbf523
Olly Seber <olly_s@hotmail.co.uk>
2022-06-05 14:42:30 -0700

    Solve 06-leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function(date) {
+    if (date % 4 == 0 && (date % 400 == 0 || date % 100 != 0)) {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
