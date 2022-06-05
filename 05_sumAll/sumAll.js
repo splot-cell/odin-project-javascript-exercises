0419b450072d814be7687638fd20bb3e133daa48
Olly Seber <olly_s@hotmail.co.uk>
2022-06-05 14:09:19 -0700

    Make solution reject non-integers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,5 @@
 const sumAll = function(num1, num2) {
-    if (!Number.isFinite(num1) || !Number.isFinite(num2) ||
+    if (!Number.isInteger(num1) || !Number.isInteger(num2) ||
             num1 < 0 || num2 < 0) {
         return "ERROR";
     }
