d6959a015417a2b6b0b2b59b948548ac94fe96dd
Olly Seber <olly_s@hotmail.co.uk>
2022-06-05 13:55:55 -0700

    Solve 05-sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (!Number.isFinite(num1) || !Number.isFinite(num2) ||
+            num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+    posDifference = (num1 - num2) > -1 ? (num1 - num2) : (num2 - num1);
+    return ((posDifference) + 1) * ((num1 + num2) / 2);
 };
 
 // Do not edit below this line
