e301edd48c02ea958c7ef3a662eb7bf11b479b52
Olly Seber <olly_s@hotmail.co.uk>
2022-06-05 13:22:04 -0700

    Solve 03-reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let retVal = '';
+    for(let i = string.length; i >= 0; i--) {
+        retVal += string.charAt(i);
+    }
+    return retVal;
 };
 
 // Do not edit below this line
