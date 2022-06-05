b5f88087dbd845db43dbb1d6ed0bceeb7834ddc6
Olly Seber <olly_s@hotmail.co.uk>
2022-06-05 13:39:27 -0700

    Solve first test of exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+    let index;
+    for (let i = 1; i < arguments.length; i++) {
+        index = array.findIndex((element) => (element === arguments[i]));       
+        if (index > 0) {
+            array.splice(index, 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
