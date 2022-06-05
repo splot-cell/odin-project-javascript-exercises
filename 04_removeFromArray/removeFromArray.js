607c3ecdef085cde2bc626ed6f6bc296ec8ee219
Olly Seber <olly_s@hotmail.co.uk>
2022-06-05 13:41:55 -0700

    Solve 04-removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,7 +2,7 @@ const removeFromArray = function(array) {
     let index;
     for (let i = 1; i < arguments.length; i++) {
         index = array.findIndex((element) => (element === arguments[i]));       
-        if (index > 0) {
+        if (index >= 0) {
             array.splice(index, 1);
         }
     }
