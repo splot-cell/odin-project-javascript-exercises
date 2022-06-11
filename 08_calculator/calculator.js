7dd27e022c5818501d26d82bf1c7fa6ec47bb75f
Olly Seber <olly_s@hotmail.co.uk>
2022-06-11 16:45:57 -0700

    Solve 08-calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let total = 0;
+  for (let i = 0; i < arr.length; i++) {
+    total += arr[i];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let retVal = 1;
+  for (let i = 0; i < arr.length; i++) {
+    retVal *= arr[i];
+  }
+  return retVal;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	let retVal = 1;
+  for (let i = 0; i < b; i++) {
+    retVal *= a;
+  }
+  return retVal;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let retVal = 1;
+	for (i = a; i > 0; i--) {
+    retVal *= i;
+  }
+  return retVal;
 };
 
 // Do not edit below this line
