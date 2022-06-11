db9e91967b8cef03bc848bbf5ec3ddd8a2ba2873
Olly Seber <olly_s@hotmail.co.uk>
2022-06-11 16:53:01 -0700

    Implement for single word

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    // remove caps
+    string.toLowerCase();
+    // remove spaces
+    // remove punc
+    // loop from front and back, checking each char
+    for(let i = 0; i < string.length; i++) {
+        if (string.charAt(i) !== string.charAt(string.length - 1 - i)) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
