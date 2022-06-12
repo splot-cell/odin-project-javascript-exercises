9770fd8af7156cf3ac40d0874c16b32e75d17149
Olly Seber <olly_s@hotmail.co.uk>
2022-06-11 19:14:04 -0700

    Implement basic functionality

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,17 @@
-const caesar = function() {
+function isLetter(char) {
+    return (char >= "a" && char <= "z") || (char >= "A" && char <= "Z");
+}
 
+const caesar = function(string, shift) {
+    let coded = "";
+    for (let i = 0; i < string.length; i++) {
+        if (isLetter(string.charAt(i))) {
+            coded += String.fromCharCode(string.charCodeAt(i) + shift);
+        } else {
+            coded += string.charAt(i);
+        }
+    }
+    return coded;
 };
 
 // Do not edit below this line
