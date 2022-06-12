b54da5d0c95816c69d386e7a31369013956729e3
Olly Seber <olly_s@hotmail.co.uk>
2022-06-11 19:03:05 -0700

    Solve 12-findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest = people.reduce((oldest, person) => {
+        let personAge;
+        if ("yearOfDeath" in person) {
+            personAge = person.yearOfDeath - person.yearOfBirth;
+        } else {
+            const thisYear = (new Date).getFullYear();
+            personAge = thisYear - person.yearOfBirth;
+        }
+        if (personAge > oldest.age) {
+            oldest.person = person;
+            oldest.age = personAge;
+        }
+        return oldest;
+    }, {person: {}, age: 0});
+    return oldest.person;
 };
 
 // Do not edit below this line
