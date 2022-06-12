4e0a64056d5d6c2623e51cb21ad636238eba8245
Olly Seber <olly_s@hotmail.co.uk>
2022-06-11 17:12:58 -0700

    Filter to only compare letters

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,11 +1,22 @@
+function filterToLetters(string) {
+    let retVal = "";
+    for (let i = 0; i < string.length; i++) {
+        if (string.charAt(i) >= "a" && string.charAt(i) <= "z" ) {
+            retVal += string.charAt(i);
+        }
+    }
+    return retVal;
+}
+
 const palindromes = function (string) {
     // remove caps
-    string.toLowerCase();
     // remove spaces
     // remove punc
+    let filteredString = filterToLetters(string.toLowerCase());
+
     // loop from front and back, checking each char
-    for(let i = 0; i < string.length; i++) {
-        if (string.charAt(i) !== string.charAt(string.length - 1 - i)) {
+    for (let i = 0; i < filteredString.length; i++) {
+        if (filteredString.charAt(i) !== filteredString.charAt(filteredString.length - 1 - i)) {
             return false;
         }
     }
