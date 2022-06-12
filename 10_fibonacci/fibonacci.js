aaeb7e4fe7f1c0aea2973a6ef6fecb98c929ef90
Olly Seber <olly_s@hotmail.co.uk>
2022-06-11 18:22:50 -0700

    Solve 10-fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 1) return "OOPS";
+    if (n < 3) return 1;
 
+    let x1 = 0;
+    let x2 = 1;
+    let sum;
+
+    for (let i = 1; i < n; i++) {
+        sum = x1 + x2;
+        x1 < x2 ? x1 = sum : x2 = sum;
+    }
+    return sum;
 };
 
 // Do not edit below this line
